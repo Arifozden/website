6c2299d4f6a0a25951cc6311a9db1eaba75ae7e7
arifozden <arifozden1@gmail.com>
2023-09-11 10:52:06 +0200

    AdminPanel ferdig og brukernavn system began

---
+++ donerm/sd/DonerSD/DonerSD/Models/Login.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace DonerSD.Models
+{
+    public class Login
+    {
+        [Required]
+        [DisplayName("Brukernavn")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DisplayName("Passord")]
+        public string Password { get; set; }
+
+        [DisplayName("Husk Meg")]
+        public bool RememberMe { get; set; }
+    }
+}
---
+++ donerm/sd/DonerSD/DonerSD/Models/Register.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Xunit;
+
+namespace DonerSD.Models
+{
+    public class Register
+    {
+        [Required]
+        [DisplayName("Navn")]
+        public string Name { get; set; }
+
+        [Required]
+        [DisplayName("Etternavn")]
+        public string SurName { get; set; }
+
+        [Required]
+        [DisplayName("Brukernavn")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DisplayName("E-post")]
+        [EmailAddress(ErrorMessage = "E-post adresse er ikke godkjent!")]
+        public string Email { get; set; }
+
+        [Required]
+        [DisplayName("Passord")]
+        public string Password { get; set; }
+
+        [Required]
+        [DisplayName("Password igjen")]
+        [System.ComponentModel.DataAnnotations.Compare("Password", ErrorMessage="passord ikke samme")]
+        public string RePassword { get; set; }
+    }
+}

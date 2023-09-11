4b0322525018f18172c27753b2b3128be055b555
arifozden <arifozden1@gmail.com>
2023-09-11 02:45:53 +0200

    admin panel

--- donerm/sd/DonerSD/DonerSD/Entity/Category.cs
+++ donerm/sd/DonerSD/DonerSD/Entity/Category.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,7 +11,11 @@ namespace DonerSD.Entity
     {
         public int Id { get; set; }
 
+        [DisplayName("Kategori Navn")]
+        [StringLength(20,ErrorMessage = "maksimalt 20 tegn")]
         public string Name { get; set; }
+
+        [DisplayName("Beskrivelse")]
         public string Description { get; set; }
 
         public List<Product> Products { get; set; }
--- donerm/sd/DonerSD/DonerSD/Entity/Product.cs
+++ donerm/sd/DonerSD/DonerSD/Entity/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -9,12 +10,19 @@ namespace DonerSD.Entity
     {
         public int Id { get; set; }
 
+        [DisplayName("Produktnavn")]
         public string Name { get; set; }
+        [DisplayName("Produktbeskrivelse")]
         public string Description { get; set; }
+        [DisplayName("Pris")]
         public double Price { get; set; }
+        [DisplayName("Lager status")]
         public int Stock { get; set; }
+        [DisplayName("Bilde")]
         public string Image { get; set; }
+        [DisplayName("PåHjemmeside")]
         public bool IsHome { get; set; }
+        [DisplayName("Godkjent")]
         public bool IsApproved { get; set; }
 
         public int CategoryId { get; set; }

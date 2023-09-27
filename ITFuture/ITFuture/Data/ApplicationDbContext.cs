ed5cb425a278b61c96e710f71873fde8da5a6629
arifozden <arifozden1@gmail.com>
2023-09-27 10:20:13 +0200

    Website, from beginning to models, mvc, database ok

---
+++ ITFuture/ITFuture/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ITFuture.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITFuture.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Answer> Answers { get; set; }
+    }
+}
